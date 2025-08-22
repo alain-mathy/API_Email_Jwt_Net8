c9f812060b2b5ed5cd781125b29ebbc393a3bd45
alain-mathy <alain_mathy@hotmail.com>
2025-08-22 08:42:49 +0200

    Add AppDbContext

---
+++ API_Email_Jwt_Net8/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Email_Jwt_Net8.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
